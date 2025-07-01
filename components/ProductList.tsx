2c11445c84fd1dd144b5bef6b15606cc7cd802bb
dn0ne <mmmdanone04@gmail.com>
2025-07-01 09:52:19 +0300

    implement searching through the list

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,23 +1,39 @@
+"use client";
+
 import Stripe from "stripe";
 import ProductCard from "./ProductCard";
+import { ChangeEvent, useState } from "react";
 
 interface Props {
   products: Stripe.Product[];
 }
 
 const ProductList = ({ products }: Props) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredProducts = products.filter((product) => {
+    const term = searchTerm.toLowerCase();
+    const nameMatch = product.name.toLowerCase().includes(term);
+    const descriptionMatch = product.description
+      ? product.description.toLowerCase().includes(term)
+      : false;
+
+    return nameMatch || descriptionMatch;
+  });
+
   return (
     <div>
       <div className="mb-6 flex justify-center">
         <input
           type="text"
           placeholder="Search products..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
           className="w-full max-w-md rounded border border-gray-300 px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
         />
       </div>
 
       <ul className="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-        {products.map((product, key) => (
+        {filteredProducts.map((product, key) => (
           <li key={key}>
             <ProductCard product={product} />
           </li>
